49070fc1d8a0173680daf1f95c88ac52b1e66224
Knlss <138739462+Knlss@users.noreply.github.com>
2024-10-01 10:31:15 -0300

    Add files via upload

---
+++ site/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Em que data houve a declaração da independência do Brasil?',
+    '7 de setembro de 1822.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'História',
+    'Quando Cristóvão Colombo chegou às Américas?',
+    '12 de outubro de 1492.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais os 6 continentes?',
+    'África, Ásia, Europa, Oceania, América e Antártida.'
+)
